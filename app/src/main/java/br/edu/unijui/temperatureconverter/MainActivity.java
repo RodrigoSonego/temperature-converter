2fee0ed6d05abad6e7474cb094b507c7f422d19f
Rodrigo Sonego <rodrigoasonego@gmail.com>
2023-04-04 21:46:07 -0300

    update: methods to convert temperatures

--- app/src/main/java/br/edu/unijui/temperatureconverter/MainActivity.java
+++ app/src/main/java/br/edu/unijui/temperatureconverter/MainActivity.java
@@ -3,11 +3,11 @@ package br.edu.unijui.temperatureconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.SpinnerAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
 
         setComponents();
         setComboBoxes();
+
+        setupButton();
     }
 
     private void setComponents() {
@@ -44,5 +46,40 @@ public class MainActivity extends AppCompatActivity {
         cbOutput.setAdapter(new ArrayAdapter<>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, temperatureLabels));
     }
 
-    
+    private void setupButton() {
+        submitButton.setOnClickListener(view -> System.out.println("cricou"));
+    }
+
+    private float celsiusToFarenheit(float celsius) {
+        float farenheit = (celsius * 1.8f) + 32;
+
+        return farenheit;
+    }
+
+    private float celsiusToKelvin(float celsius) {
+        return celsius + 273.15f;
+    }
+
+    private float kelvinToCelsius(float kelvin) {
+        return  kelvin - 273.15f;
+    }
+
+    private float kelvinToFarenheit(float kelvin) {
+        float farenheint = ((kelvin - 273.15f) * 1.8f) + 32;
+
+        return farenheint;
+    }
+
+    private float farenheitToCelsius(float far) {
+        float celsius = (far - 32) * (5/9f);
+
+        return celsius;
+    }
+
+    private float farenheitToKelvin(float far) {
+        float kelvin = (far - 32) * (5/9f) + 273.15f;
+
+        return kelvin;
+    }
+
 }
